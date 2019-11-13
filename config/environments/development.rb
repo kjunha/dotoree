cf2806bd26709967bd4d582602d49448de8cffb0
Junha Kim <kjunha77@gmail.com>
2019-11-13 00:55:39 +0000

    add controller, bootstrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,10 +52,11 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
+  
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
-
+  # Add host for development
+  config.hosts << "63db492c8a644505b2f1db80fb3d1449.vfs.cloud9.us-east-1.amazonaws.com"
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
