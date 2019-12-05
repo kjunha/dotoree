4e83684de9e529eccc6c12670ea0c2b3c6b9dd0f
Junha Kim <kjunha77@gmail.com>
2019-12-05 13:26:45 +0000

    add model test

--- test/models/square_test.rb
+++ test/models/square_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class SquareTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid square" do
+    square = FactoryBot.build :square
+    assert square.valid?
+  end
+  
+  test "invalid square without name" do
+    square = FactoryBot.build :square
+    square.name = nil
+    
+    refute square.valid?
+  end
+  
+  test "invalid square without user" do
+    square = FactoryBot.build :square
+    square.user = nil
+    
+    refute square.valid?
+  end
 end
